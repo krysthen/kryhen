d241b34be97fb6f2d6bda215a8aac0e96693d8ad
KrystianH <87086006+krysthen@users.noreply.github.com>
2024-03-04 18:17:14 +0100

    SpeedInsights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Home from './pages/index'
 import { initGA, logPageView } from '../app/utils/analytics';
 import React, { useState, useEffect } from 'react';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Page() {
 
@@ -17,6 +18,7 @@ export default function Page() {
         <>
             <Home/>
             <Analytics/> 
+            <SpeedInsights />
         </>
 
 
